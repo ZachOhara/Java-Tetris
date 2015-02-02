c06958ab25d371e3242272bf55f2b17bbc217c17
Zach Ohara <zjohara@gmail.com>
2015-02-01 22:42:25 -0800

    Repackaged GameManager

---
+++ src/io/github/zachoahra/javatetris/management/GameManager.java
@@ -0,0 +1,12 @@
+package io.github.zachoahra.javatetris.management;
+
+import io.github.zachoahra.javatetris.game.BlockGrid;
+
+public class GameManager {
+	
+	private BlockGrid gameGrid;
+	
+	private static final int gridWidth = 10;
+	private static final int gridHeight = 10;
+
+}
