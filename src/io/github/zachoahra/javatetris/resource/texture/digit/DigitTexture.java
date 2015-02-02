5be6266e48def5e1088502b70844c2f08cb525be
Zach Ohara <zjohara@gmail.com>
2015-02-01 23:20:12 -0800

    Added DigitTexture.get(int)
    Returns the digit texture corresponding to the given digit. If the given
    number is not a digit, returns null.

--- src/io/github/zachoahra/javatetris/resource/texture/digit/DigitTexture.java
+++ src/io/github/zachoahra/javatetris/resource/texture/digit/DigitTexture.java
@@ -14,7 +14,6 @@ import javax.swing.JLabel;
 
 public enum DigitTexture {
 	
-	NULL (),
 	D0 ("0.png"),
 	D1 ("1.png"),
 	D2 ("2.png"),
@@ -40,16 +39,39 @@ public enum DigitTexture {
 		this.texture = scale(temp, NumberImage.getDigitWidthPix(), NumberImage.getDigitHeightPix());
 	}
 
-	private DigitTexture() {
-		this.texture = null;
-	}
-
 	public ImageIcon getImage() {
 		return new ImageIcon(this.texture);
 	}
 
 	public JLabel getLabel() {
 		return new JLabel(this.getImage());
+	}
+	
+	public static DigitTexture get(int i) {
+		switch(i) {
+		case 0:
+			return DigitTexture.D0;
+		case 1:
+			return DigitTexture.D1;
+		case 2:
+			return DigitTexture.D2;
+		case 3:
+			return DigitTexture.D3;
+		case 4:
+			return DigitTexture.D4;
+		case 5:
+			return DigitTexture.D5;
+		case 6:
+			return DigitTexture.D6;
+		case 7:
+			return DigitTexture.D7;
+		case 8:
+			return DigitTexture.D8;
+		case 9:
+			return DigitTexture.D9;
+		default:
+			return null;
+		}
 	}
 
 	public static BufferedImage scale(BufferedImage b, int newW, int newH) {
@@ -61,16 +83,16 @@ public enum DigitTexture {
 		g.dispose();
 		return db;
 	}
+	
+	public String toString() {
+		return this.filename.substring(0, this.filename.length() - 4);
+	}
 
 	public static void main(String[] args) {
 		JFrame f = new JFrame();
 		f.setLayout(new FlowLayout());
 		f.add(DigitTexture.D0.getLabel());
 		f.setVisible(true);
-	}
-	
-	public String toString() {
-		return this.filename.substring(0, this.filename.length() - 4);
 	}
 
 }
