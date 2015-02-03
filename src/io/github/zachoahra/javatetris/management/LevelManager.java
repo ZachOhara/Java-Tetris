669f2c331d7c67f1d676266df79f50d8d6f2d661
Zach Ohara <zjohara@gmail.com>
2015-02-02 18:32:25 -0800

    Finished LevelManager

--- src/io/github/zachoahra/javatetris/management/LevelManager.java
+++ src/io/github/zachoahra/javatetris/management/LevelManager.java
@@ -3,9 +3,13 @@ package io.github.zachoahra.javatetris.management;
 public class LevelManager {
 	
 	private int level;
+	private int dropSpeed; // in frames per line @ 60fps
+	
+	private static final int[] linePoints = {0, 40, 100, 300, 1200};
 	
 	public LevelManager(int level) {
 		this.level = level;
+		this.dropSpeed = 48;
 	}
 	
 	public LevelManager() {
@@ -14,11 +18,30 @@ public class LevelManager {
 	
 	public void levelUp() {
 		this.level++;
+		if (this.level <= 8)
+			this.dropSpeed -= 5;
+		else if (this.level == 9)
+			this.dropSpeed = 6;
+		else if (this.level <= 12)
+			this.dropSpeed = 5;
+		else if (this.level <= 15)
+			this.dropSpeed = 4;
+		else if (this.level <= 18)
+			this.dropSpeed = 3;
+		else if (this.level <= 28)
+			this.dropSpeed = 2;
+		else
+			this.dropSpeed = 1;
+	}
+	
+	public int getLinePoints(int lines) {
+		if (lines < 0 || lines > 4)
+			return 0;
+		return linePoints[lines] * (this.level + 1);
 	}
 	
 	public int getDropTicks() {
-		return 0;
-		//TODO: return the milliseconds for the block to fall at the current level
+		return (int)(((double)this.dropSpeed / 600) * 1000);
 	}
 
 }
