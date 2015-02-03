f81516da9b23259e924e15b0a8bbcb5ada510827
Zach Ohara <zjohara@gmail.com>
2015-02-03 12:08:20 -0800

    Fixed lots of problems in GameManager

--- src/io/github/zachoahra/javatetris/management/GameManager.java
+++ src/io/github/zachoahra/javatetris/management/GameManager.java
@@ -3,6 +3,7 @@ package io.github.zachoahra.javatetris.management;
 import java.util.Arrays;
 
 import io.github.zachoahra.javatetris.game.BlockGrid;
+import io.github.zachoahra.javatetris.game.ShapeFactory;
 import io.github.zachoahra.javatetris.input.KeyboardInputListener;
 import io.github.zachoahra.javatetris.window.GameWindow;
 
@@ -13,7 +14,7 @@ public class GameManager {
 	private KeyboardInputListener keyListener;
 	
 	private static final int gridWidth = 10;
-	private static final int gridHeight = 10;
+	private static final int gridHeight = 17;
 	private static final int[] inputLeft = {65, 37};
 	private static final int[] inputRight = {68, 39};
 	private static final int[] inputUp = {87, 38};
@@ -22,21 +23,36 @@ public class GameManager {
 	public GameManager(GameWindow master) {
 		this.masterWindow = master;
 		this.gameGrid = new BlockGrid(gridWidth, gridHeight);
-		this.keyListener = new KeyboardInputListener();
-		this.keyListener.setMaster(this.masterWindow);
+		this.gameGrid.setLocation(0, 0);
+		this.keyListener = new KeyboardInputListener(this.masterWindow, this);
+		this.masterWindow.addKeyListener(this.keyListener);
+		this.gameGrid.setShape(ShapeFactory.makeRandomShape());
+		this.masterWindow.add(this.gameGrid);
 	}
 	
 	public void doInput(int code) {
+		//TODO: make this less ugly, try to use list.contains()
+		//System.out.println("jdjd");
 		// left
-		if (Arrays.asList(inputLeft).contains(code))
-			if (this.gameGrid.canShapeShift(-1))
+		//if (Arrays.asList(inputLeft).contains(code))
+		if (code == inputLeft[0] || code == inputLeft[1])
+			if (this.gameGrid.canShapeShift(-1)) {
 				this.gameGrid.shiftShape(-1);
+				System.out.println("left");
+			}
 		// right
-		if (Arrays.asList(inputRight).contains(code))
-			if (this.gameGrid.canShapeShift(1))
+		if (code == inputRight[0] || code == inputRight[1])
+			if (this.gameGrid.canShapeShift(1)) {
 				this.gameGrid.shiftShape(1);
+				System.out.println("right");
+			}
 		//TODO: down for soft drop
 		//TODO: up for firm drop
+	}
+	
+	public static void main(String[] args) {
+		GameWindow g = new GameWindow();
+		g.setVisible(true);
 	}
 
 }
