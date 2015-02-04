db28b370d7b0ab1e85cbec523b34283ffbd480b9
Zach Ohara <zjohara@gmail.com>
2015-02-03 22:10:19 -0800

    Wrote full TimeManager

--- src/io/github/zachoahra/javatetris/management/TimeManager.java
+++ src/io/github/zachoahra/javatetris/management/TimeManager.java
@@ -1,11 +1,31 @@
 package io.github.zachoahra.javatetris.management;
 
-public class TimeManager implements Runnable {
+public class TimeManager extends Thread {
+
+	private LevelManager level;
+	private GameManager game;
+	
+	private static final double fastFactor = 0.05 ;
+
+	public TimeManager(LevelManager level, GameManager game) {
+		super();
+		this.level = level;
+		this.game = game;
+	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		while(!this.isInterrupted()) {
+			int time = 0;
+			try {
+				if (this.game.isFastMode())
+					time = (int)(level.getDropTicks() * fastFactor);
+				else
+					time = level.getDropTicks();
+				Thread.sleep(time);
+			} catch (InterruptedException expected) {}
+			game.doGravity();
+		}
 	}
 
 }
