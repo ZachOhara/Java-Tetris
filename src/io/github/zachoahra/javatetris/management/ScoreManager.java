77f1923b398a006e304ca5a19b28bc690c5bee06
Zach Ohara <zjohara@gmail.com>
2015-02-03 22:10:30 -0800

    Wrote template ScoreManager

---
+++ src/io/github/zachoahra/javatetris/management/ScoreManager.java
@@ -0,0 +1,15 @@
+package io.github.zachoahra.javatetris.management;
+
+import io.github.zachoahra.javatetris.score.NumberImage;
+import io.github.zachoahra.javatetris.window.TitleImage;
+
+public class ScoreManager {
+	
+	private TitleImage scoreTitle;
+	private TitleImage linesTitle;
+	private TitleImage levelTitle;
+	private NumberImage scoreCounter;
+	private NumberImage linesCounter;
+	private NumberImage levelCounter;
+
+}
