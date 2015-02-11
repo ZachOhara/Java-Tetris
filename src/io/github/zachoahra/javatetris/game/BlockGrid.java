87cbb44acb3909888805c85a7a0f17a1a9b9f7cf
Zach Ohara <zjohara@gmail.com>
2015-02-10 23:22:00 -0800

    BlockGrid updates...
    --Public methods now make sure that the current shape is not null before
    proceeding
    --Size is calculated statically now, and there are no instance
    references to size.

--- src/io/github/zachoahra/javatetris/game/BlockGrid.java
+++ src/io/github/zachoahra/javatetris/game/BlockGrid.java
@@ -1,92 +1,118 @@
 package io.github.zachoahra.javatetris.game;
 
+import java.awt.Dimension;
 import java.util.LinkedList;
 
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class BlockGrid extends JPanel {
-
-	private int width;
-	private int height;
+	
 	private Shape currentShape;
 	private Block[][] blockgrid;
 	
+	private static final int xPos = 0;
+	private static final int yPos = 0;
+	private static final int gridWidth = 10;
+	private static final int gridHeight = 17;
+
 	private static final long serialVersionUID = 1L;
-	
-	public BlockGrid(int width, int height) {
+
+	public BlockGrid(JFrame window) {
 		super();
-		this.width = width;
-		this.height = height;
-		this.blockgrid = new Block[height][width];
-		this.setSize(width * Block.getLength(), height * Block.getLength());
+		this.blockgrid = new Block[gridHeight][gridWidth];
+		this.setSize(getStaticSize());
+		this.setLocation(xPos, yPos);
 		this.setLayout(null);
+		window.add(this);
 	}
-	
+
+	public Block[][] getBlockgridArray() {
+		return this.blockgrid;
+	}
+
 	public Shape getCurrentShape() {
 		return this.currentShape;
 	}
-	
+
 	public void setShape(Shape s) {
 		this.currentShape = null;
 		this.currentShape = s;
 		s.setPanel(this);
 		this.update();
 	}
-	
+
 	public boolean descendShape() {
-		if (this.isTranslationViable(0, 1))
-			this.currentShape.translate(0, 1);
-		else
+		if (this.currentShape != null) {
+			if (this.isTranslationViable(0, 1))
+				this.currentShape.translate(0, 1);
+			else
+				return false;
+			this.update();
+			return true;
+		} else
 			return false;
-		this.update();
-		return true;
 	}
-	
+
 	public boolean shiftShape(int d) {
-		if (this.isTranslationViable(d, 0))
-			this.currentShape.translate(d, 0);
-		else
+		if (this.currentShape != null) {
+			if (this.isTranslationViable(d, 0))
+				this.currentShape.translate(d, 0);
+			else
+				return false;
+			this.update();
+			return true;
+		} else
 			return false;
-		this.update();
-		return true;
 	}
-	
+
 	public boolean rotateShape(int d) {
-		if (this.isTranslationViable(this.currentShape.testRotate(d), 0, 0))
-			this.currentShape.rotate(d);
-		else
+		if (this.currentShape != null) {
+			if (this.isTranslationViable(this.currentShape.testRotate(d), 0, 0))
+				this.currentShape.rotate(d);
+			else
+				return false;
+			this.update();
+			return true;
+		} else
 			return false;
-		this.update();
-		return true;
 	}
-	
+
 	public void hardDrop() {
-		int i = 0;
-		while (this.isTranslationViable(0, i))
-			i++;
-		this.currentShape.translate(0, i - 1);
-		this.anchorShape();
+		if (this.currentShape != null) {
+			int i = 0;
+			while (this.isTranslationViable(0, i))
+				i++;
+			this.currentShape.translate(0, i - 1);
+			this.anchorShape();
+			this.currentShape = null;
+		}
 	}
-	
+
 	public void anchorShape() {
-		Block[][] grid = this.currentShape.getBlockGrid();
-		int x = this.currentShape.getXPos();
-		int y = this.currentShape.getYPos();
-		for (int i = 0; i < 4; i++)
-			for (int j = 0; j < 4; j++)
-				if (grid[i][j] != null)
-					this.blockgrid[i+y][j+x] = grid[i][j];
-		this.update();
+		if (this.currentShape != null) {
+			Block[][] grid = this.currentShape.getBlockGrid();
+			int x = this.currentShape.getXPos();
+			int y = this.currentShape.getYPos();
+			for (int i = 0; i < 4; i++)
+				for (int j = 0; j < 4; j++)
+					if (grid[i][j] != null)
+						this.blockgrid[i+y][j+x] = grid[i][j];
+			this.update();
+		}
 	}
-	
+
 	public boolean isShapeViable() {
-		return this.isTranslationViable(0, 0);
+		if (this.currentShape == null)
+			return true;
+		else
+			return this.isTranslationViable(0, 0);
 	}
-	
+
 	private boolean isTranslationViable(int dx, int dy) {
 		return this.isTranslationViable(this.currentShape.getBlockGrid(), dx, dy);
 	}
-	
+
 	private boolean isTranslationViable(Block[][] prospective, int dx, int dy) {
 		int x = dx + this.currentShape.getXPos();
 		int y = dy + this.currentShape.getYPos();
@@ -97,7 +123,7 @@ public class BlockGrid extends JPanel {
 				if (prospective[i][j] != null) {
 					xN = j + x;
 					// if new coordinate is out of range (don't check for y < 0)
-					if (!(yN < this.height) || !(0 <= xN && xN < this.width))
+					if (!(yN < gridHeight) || !(0 <= xN && xN < gridWidth))
 						return false;
 					// if prospective space is alread occupied (check for y<0 here)
 					if (yN >= 0 && this.blockgrid[yN][xN] != null)
@@ -106,7 +132,7 @@ public class BlockGrid extends JPanel {
 		}
 		return true;
 	}
-	
+
 	public void removeAll(int[] lines) {
 		int correction = 0;
 		for (int i : lines) {
@@ -115,49 +141,61 @@ public class BlockGrid extends JPanel {
 		}
 		this.update();
 	}
-	
+
 	private void clearLine(int l) {
-		for (int i = 0; i < this.width; i++) {
+		for (int i = 0; i < gridWidth; i++) {
 			if (this.blockgrid[l][i] != null)
 				this.remove(this.blockgrid[l][i]);
 			this.blockgrid[l][i] = null;
 		}
 		for (int i = l - 1; i >= 0; i--) {
-			for (int j = 0; j < this.width; j++) {
+			for (int j = 0; j < gridWidth; j++) {
 				if (this.blockgrid[i][j] != null)
 					this.blockgrid[i][j].translate(0, 1);
 				this.blockgrid[i + 1][j] = this.blockgrid[i][j];
 			}
 		}
 	}
-	
+
 	public int[] getFullLines() {
 		LinkedList<Integer> list = new LinkedList<Integer>();
-		for (int i = this.height - 1; i >= 0; i--) {
+		for (int i = gridHeight - 1; i >= 0; i--) {
 			if (this.lineIsFull(i))
 				list.add(i);
 		}
 		Integer[] temp = list.toArray(new Integer[list.size()]);
 		return toIntArray(temp);
 	}
-	
+
 	private boolean lineIsFull(int l) {
 		for (Block b : this.blockgrid[l])
 			if (b == null)
 				return false;
 		return true;
 	}
-	
+
 	private void update() {
 		this.revalidate();
 		this.repaint();
 	}
-	
+
 	private static int[] toIntArray(Integer[] intArr) {
 		int[] result = new int[intArr.length];
 		for (int i = 0; i < result.length; i++)
 			result[i] = intArr[i];
 		return result;
+	}
+	
+	public static Dimension getStaticSize() {
+		return new Dimension(getStaticWidth(), getStaticHeight());
+	}
+	
+	public static int getStaticWidth() {
+		return Block.getLength() * gridWidth;
+	}
+	
+	public static int getStaticHeight() {
+		return Block.getLength() * gridHeight;
 	}
 
 }
