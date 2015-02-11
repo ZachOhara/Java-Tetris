40c3f7cac8f1cc4b5c2c1c9009d781bd79f1fb8e
Zach Ohara <zjohara@gmail.com>
2015-02-10 23:08:16 -0800

    JavaPlugin now implements GameController

--- src/io/github/zachoahra/javatetris/plugin/JavaPlugin.java
+++ src/io/github/zachoahra/javatetris/plugin/JavaPlugin.java
@@ -1,36 +1,78 @@
 package io.github.zachoahra.javatetris.plugin;
 
+import io.github.zachoahra.javatetris.game.Block;
 import io.github.zachoahra.javatetris.game.BlockGrid;
 import io.github.zachoahra.javatetris.game.Shape;
+import io.github.zachoahra.javatetris.input.GameController;
 import io.github.zachoahra.javatetris.management.GameManager;
-import io.github.zachoahra.javatetris.window.GameWindow;
 
-public abstract class JavaPlugin {
+public abstract class JavaPlugin implements GameController {
 	
-	private GameWindow window = null;
+	private GameManager game;
+	private boolean hasControl;
 	
-	public void setWindow(GameWindow window) {
-		this.window = window;
+	public JavaPlugin() {
+		this.game = null;
+		this.hasControl = false;
 	}
 	
-	public GameWindow getWindow() {
-		return this.window;
+	@Override
+	public boolean hasControl() {
+		return this.hasControl;
+	}
+	
+	@Override
+	public void setControl(boolean b) {
+		this.hasControl = b;
+	}
+	
+	@Override
+	public void setGame(GameManager game) {
+		this.game = null;
 	}
 	
 	public GameManager getGame() {
-		return this.window.getGame();
+		return this.game;
+	}
+	
+	public boolean canControlGame() {
+		return this.game.isGameRunning() && this.hasControl();
 	}
 	
 	public BlockGrid getBlockGrid() {
-		return this.window.getGame().getBlockGrid();
+		return this.getGame().getBlockGrid();
+	}
+	
+	public Block[][] getBlockGridArray() {
+		return this.getBlockGrid().getBlockgridArray();
 	}
 	
 	public Shape getCurrentShape() {
-		return this.window.getGame().getBlockGrid().getCurrentShape();
+		return this.getGame().getBlockGrid().getCurrentShape();
+	}
+	
+	public Block[][] getCurrentShapeArray() {
+		return this.getCurrentShape().getBlockGrid();
 	}
 	
 	public Shape getNextShape() {
-		return this.window.getGame().getNextShape();
+		return this.getGame().getNextShapePanel().read();
+	}
+	
+	public Block[][] getNextShapeArray() {
+		return this.getNextShape().getBlockGrid();
+	}
+	
+	public void shiftShape(int dir) {
+		this.getBlockGrid().shiftShape(dir);
+	}
+	
+	public void rotateShape(int dir) {
+		this.getBlockGrid().rotateShape(dir);
+	}
+	
+	public void hardDropShape() {
+		this.getBlockGrid().hardDrop();
 	}
 
 }
