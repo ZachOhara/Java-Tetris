bd28719db90b56d6c02a0ad2b675a4073ef8862c
Zach Ohara <zjohara@gmail.com>
2015-02-10 23:31:45 -0800

    Refactored PluginLoader

--- src/io/github/zachoahra/javatetris/plugin/PluginLoader.java
+++ src/io/github/zachoahra/javatetris/plugin/PluginLoader.java
@@ -1,14 +1,11 @@
 package io.github.zachoahra.javatetris.plugin;
 
 import java.io.File;
+import java.lang.instrument.IllegalClassFormatException;
 import java.lang.reflect.Constructor;
-import java.net.JarURLConnection;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.concurrent.CancellationException;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
-import java.util.stream.Stream;
 
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -16,67 +13,78 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class PluginLoader {
 	
-	public JavaPlugin loadPlugin() throws Exception {
-		JFrame f = new JFrame();
-		f.setResizable(false);
-		f.setSize(500, 400);
-		f.setLocationRelativeTo(null);
-		f.setTitle("Load plugin");
-		
-		JFileChooser fc = new JFileChooser();
-		FileNameExtensionFilter fil = new FileNameExtensionFilter("*.jar", "jar");
-		fc.setFileFilter(fil);
-		
-		File fi;
-		int r = fc.showOpenDialog(f);
-		if (r == JFileChooser.APPROVE_OPTION)
-			fi = fc.getSelectedFile();
+	public static JavaPlugin handleLoadPlugin() {
+		JavaPlugin plugin = null;
+		try {
+			plugin = loadPlugin();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (IllegalClassFormatException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return plugin;
+	}
+
+	public static JavaPlugin loadPlugin() throws ClassNotFoundException,
+				NoSuchMethodException, IllegalAccessException,
+				IllegalClassFormatException, Exception {
+		JFrame dialogFrame = new JFrame();
+		dialogFrame.setResizable(false);
+		dialogFrame.setSize(500, 400);
+		dialogFrame.setLocationRelativeTo(null);
+		dialogFrame.setTitle("Load plugin");
+
+		JFileChooser dialogBody = new JFileChooser();
+		FileNameExtensionFilter jarFilter = new FileNameExtensionFilter("*.jar", "jar");
+		dialogBody.setFileFilter(jarFilter);
+
+		File selectedFile;
+		int dialogResult = dialogBody.showOpenDialog(dialogFrame);
+		if (dialogResult == JFileChooser.APPROVE_OPTION)
+			selectedFile = dialogBody.getSelectedFile();
 		else
 			throw new CancellationException();
-		System.out.println(fi.getName());
-		
-		return this.loadPlugin(fi.toURI().toURL());
-	}
-	
-	private JavaPlugin loadPlugin(URL url) throws Exception {
-		URL[] urlArr = {url};
-		URLClassLoader ucl = new URLClassLoader(urlArr);
 
-		Class<?> cl = ucl.loadClass("Robot");
-		
-		Constructor<?> cons = cl.getConstructor(new Class<?>[0]);
-		Object ob = cons.newInstance(new Object[0]);
-		if (ob instanceof JavaPlugin)
-			return (JavaPlugin)ob;
+		return loadPlugin(selectedFile.toURI().toURL());
+	}
+
+	/**
+	 * 
+	 * @param url
+	 * @return
+	 * @throws ClassNotFoundException if there is no Robot class defined in
+	 * 		the default package of the imported jar
+	 * @throws NoSuchMethodException if there is no default constructor for
+	 * 		the Robot class
+	 * @throws IllegalAccessException if the default constructor for Robot is not visible
+	 * @throws IllegalClassFormatException if the found Robot class does
+	 * 		not subclass JavaPlugin
+	 * @throws Exception if an error is caused by the back-end, and is not
+	 * 		necessarily the result of the plugin being incorrect
+	 */
+	private static JavaPlugin loadPlugin(URL url) throws ClassNotFoundException,
+				NoSuchMethodException, IllegalAccessException,
+				IllegalClassFormatException, Exception {
+		URL[] urlArray = {url};
+		URLClassLoader loader = new URLClassLoader(urlArray);
+		Class<?> targetClass = loader.loadClass("Robot");
+		loader.close();
+		Constructor<?> defaultConstructor = targetClass.getConstructor(new Class<?>[0]);
+		Object targetObject = defaultConstructor.newInstance(new Object[0]);
+		if (targetObject instanceof JavaPlugin)
+			return (JavaPlugin)targetObject;
 		else
-			throw new Exception("Class is not an instance of JavaPlugin");
+			throw new IllegalClassFormatException("Robot is not an instance of JavaPlugin");
 	}
-	
-	private JavaPlugin loadPlugin(File file) throws Exception {
-		JarFile jf = new JarFile(file);
-		String loadedClass = jf.getJarEntry("Robot.class").getName();
-		
-		for (Object je : jf.stream().toArray())
-			System.out.println(((JarEntry)je).getName());
-		
-		jf.close();
-		System.out.println(loadedClass);
-		return this.loadPlugin(loadedClass);
-		//return null;
-	}
-	
-	private JavaPlugin loadPlugin(String classname) throws Exception {
-		Class<?> cl = Class.forName(classname);
-		Constructor<?> cons = cl.getConstructor(new Class<?>[0]);
-		Object ob = cons.newInstance(new Object[0]);
-		if (ob instanceof JavaPlugin)
-			return (JavaPlugin)ob;
-		else
-			throw new Exception("Class is not an instance of JavaPlugin");
-	}
-	
+
 	public static void main(String[] args) throws Exception {
-		JavaPlugin p = new PluginLoader().loadPlugin();
+		JavaPlugin p = PluginLoader.loadPlugin();
 		System.out.println(p.toString());
 	}
 
