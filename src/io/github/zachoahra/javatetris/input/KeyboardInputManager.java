cbf6c4f21df90e57761f3cc81df4a5bd65515284
Zach Ohara <zjohara@gmail.com>
2015-02-10 23:09:40 -0800

    Made the key listener in charge of its own stuff
    The key listener will now keep a reference to the GameManager that it
    controls, but will use inputs to directly modify the block grid, instead
    of passing accepted key codes up the chain.

---
+++ src/io/github/zachoahra/javatetris/input/KeyboardInputManager.java
@@ -0,0 +1,101 @@
+package io.github.zachoahra.javatetris.input;
+
+import io.github.zachoahra.javatetris.management.GameManager;
+import io.github.zachoahra.javatetris.management.TimeManager;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Arrays;
+
+import javax.swing.JFrame;
+
+public class KeyboardInputManager implements KeyListener, GameController {
+
+	private GameManager game;
+	private TimeManager time;
+	private boolean hasControl;
+
+	private static final boolean displayOutputs = false;
+
+	private static final Integer[] inputRotateCW = {32};
+	private static final Integer[] inputRotateCCW = {16};
+	private static final Integer[] inputLeft = {65, 37};
+	private static final Integer[] inputRight = {68, 39};
+	private static final Integer[] inputUp = {87, 38};
+	private static final Integer[] inputDown = {83, 40};
+
+	public KeyboardInputManager(JFrame window, GameManager game, TimeManager time) {
+		this.setGame(game);
+		window.addKeyListener(this);
+		this.time = time;
+		this.hasControl = false;
+	}
+
+	@Override
+	public boolean hasControl() {
+		return this.hasControl;
+	}
+
+	@Override
+	public void setControl(boolean b) {
+		this.hasControl = b;
+	}
+
+	@Override
+	public void setGame(GameManager game) {
+		this.game = game;
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		int code = e.getKeyCode();
+		if (displayOutputs)
+			System.out.println(code);
+
+		if (this.hasControl && this.game.isGameRunning()) {
+			// left
+			if (Arrays.asList(inputLeft).contains(code))
+				this.game.getBlockGrid().shiftShape(-1); 
+			// right
+			if (Arrays.asList(inputRight).contains(code))
+				this.game.getBlockGrid().shiftShape(1);
+			// down
+			if (Arrays.asList(inputDown).contains(code)) {
+				this.time.setFastMode(true);
+				this.time.interrupt();
+			}
+			// up
+			if (Arrays.asList(inputUp).contains(code))
+				this.game.getBlockGrid().hardDrop();
+			// rotate clockwise
+			if (Arrays.asList(inputRotateCW).contains(code))
+				this.game.getBlockGrid().rotateShape(1);
+			// rotate counterclockwise
+			if (Arrays.asList(inputRotateCCW).contains(code))
+				this.game.getBlockGrid().rotateShape(-1);
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		int code = e.getKeyCode();
+		if (displayOutputs)
+			System.out.println(code);
+
+		if (this.hasControl)
+			if (Arrays.asList(inputDown).contains(code))
+				this.time.setFastMode(false);
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// ignore input
+	}
+
+	public static void main(String[] args) {
+		JFrame f = new JFrame();
+		new KeyboardInputManager(f, null, null);
+		f.setVisible(true);
+	}
+
+}
