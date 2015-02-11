a42c0848979043192e2503370495283da3c5d056
Zach Ohara <zjohara@gmail.com>
2015-02-10 23:07:44 -0800

    Added GameController interface
    This should help the plugin system, by making everything that controls
    the block dropping mechanism an instance of a single interface.
    Transferring control of the game between the user and plugins will be
    much simpler this way.

---
+++ src/io/github/zachoahra/javatetris/input/GameController.java
@@ -0,0 +1,11 @@
+package io.github.zachoahra.javatetris.input;
+
+import io.github.zachoahra.javatetris.management.GameManager;
+
+public interface GameController {
+		
+	public void setGame(GameManager game);
+	public boolean hasControl();
+	public void setControl(boolean b);
+
+}
